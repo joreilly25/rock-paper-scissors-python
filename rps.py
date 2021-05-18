5885da650fcb7f029bf16fe79e0cecca6e7f0c2b
Justin O'Reilly <joreilly25@gmail.com>
2021-05-18 00:45:03 -0400

    added exit and help text

--- rps.py
+++ rps.py
@@ -13,6 +13,10 @@ def userPlay():
         if playerSelection == "rock" or playerSelection == "paper" or playerSelection == "scissors":
             return playerSelection
             break
+        elif playerSelection == "exit" or playerSelection == "close":
+            break
+        elif playerSelection == "help":
+            print("Typing exit or close will stop program. if you want to play, please select rock paper or scissors.")
         
 print(userPlay())
 print(computerPlay())
