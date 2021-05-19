949c397a0b2d3be312a38506d9178a0adbdd428c
Justin O'Reilly <joreilly25@gmail.com>
2021-05-19 15:01:20 -0400

    added the game function and finished playRound()

--- rps.py
+++ rps.py
@@ -5,7 +5,7 @@ compScore = 0
 def computerPlay():
     compSelections = ["rock", "paper", "scissors"]
     compSelection = random.choice(compSelections)
-    return "rock"
+    return compSelection
 
 def userPlay():
     while True:
@@ -27,7 +27,34 @@ def playRound(playerSelection, compSelection):
     elif playerSelection == "scissors" and compSelection == "rock":
         compScore += 1
         return print("You Lose! Rock beats Scissors!")
+    elif playerSelection == "rock" and compSelection == "paper":
+        compScore += 1
+        return print("You Lose! Paper beats Rock!")
+    elif playerSelection == "paper" and compSelection == "rock":
+        playerScore += 1
+        return print("You Win! Paper beats Rock!")
+    elif playerSelection == "paper" and compSelection == "scissors":
+        compScore += 1
+        return print("You Lose! Scissors beats Paper!")
+    elif playerSelection == "scissors" and compSelection == "paper":
+        playerScore += 1
+        return print("You Win! Scissors beats Paper!")
+    else:
+        return print ("Draw!")
+
+def game():
+    while True:
+        playRound(userPlay(), computerPlay())
+        print("Your Score: " + str(playerScore))
+        print("Comp Score: " + str(compScore))
         
-playRound(userPlay(), computerPlay())
-print("Your Score: " + str(playerScore))
-print("Comp Score: " + str(compScore))
+        if playerScore >= 5:
+            return print("Congrats You Win!")
+            break
+        elif compScore >= 5:
+            return print("Oh no! Try again!")
+            break
+
+game()
+        
+# playRound(userPlay(), computerPlay())
